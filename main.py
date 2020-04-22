eaacc05d5245af699f0c61234920cd4dddc73a59
RoverRedRover <dvcasson@outlook.com>
2020-04-22 03:17:48 +0000

    initial commit included an error: open docstring on line 42 means file can't be executed without raising an error. this is fixed now. a couple other minor tweaks.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #! usr/bin/python3
 """ Provide player details about the minimum price at which he should sell his product either on the Exchange or by contract in SimCompanies. """
 
+__version__ = 2.0  # Prior code contained in "old.txt"
+
+
 class Product:
 
     xpt_mult = {
@@ -26,29 +29,35 @@ class Product:
             self.src_cost = src_cost
             self.freight = Product.xpt_cost * Product.xpt_mult[self.prod_type]
             self.freight_contr = self.freight / 2
-            self.exch_price = (self.src_cost + self.freight) / (1 - Product.exc_pct - Product.tgt_mgn)
+            self.exch_price = (self.src_cost + self.freight) / (
+                1 - Product.exc_pct - Product.tgt_mgn)
             self.exch_fee = self.exch_price * 0.03
             self.exch_cogs = self.src_cost + self.freight + self.exch_fee
-            self.contr_price = (self.src_cost + self.freight_contr) / (1 - Product.tgt_mgn)
+            self.contr_price = (self.src_cost + self.freight_contr) / (
+                1 - Product.tgt_mgn)
 
-            """with open("history.txt", "w") as text_file:
+            """TODO with open("history.txt", "w") as text_file:
                 text_file.write()"""
 
         else:
             print("Invalid product selection.")
             return
 
-    def what_if(self, price):
-        """ 
-
     def tell_all(self):
+        """ give user the full Monty """
+        print(
+            f"On the Exchange, sell your {self.prod_type} at $"
+            f"{round(self.exch_price,2)}. Your sourcing cost was $"
+            f"{self.src_cost}. Freight will cost ${round(self.freight,2)}. "
+            f"The exchange fee will be ${round(self.exch_fee,2)}. "
+            f"Your total COGS is ${round(self.exch_cogs,2)}. "
+            f"Your gross margin per unit sold will be $"
+            f"{round(self.exch_price - self.exch_cogs,2)} "
+            f"and your gross margin percentage will be "
+            f"{round((self.exch_price - self.exch_cogs)/self.exch_price * 100,2)}%."
+        )
 
-        print(f"On the Exchange, sell your {self.prod_type} "
-                f"at ${round(self.exch_price,2)}. Your sourcing cost was "
-                f"${self.src_cost}. Freight will cost ${round(self.freight,2)}. "
-                f"The exchange fee will be ${round(self.exch_fee,2)}. "
-                f"Your total COGS is ${round(self.exch_cogs,2)}. "
-                f"Your gross margin per unit sold will be ${round(self.exch_price - self.exch_cogs,2)} "
-                f"and your gross margin percentage will be "
-                f"{round((self.exch_price - self.exch_cogs)/self.exch_price * 100,2)}%.")
-
+    def what_if(self, price):
+        """ provide margin at prospective unit price """
+        #TODO
+        pass
